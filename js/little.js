8660502d36f1a5777ed98e80f4e13d17fe84b90a
Ginger <118656791+min-wii@users.noreply.github.com>
2023-12-24 23:51:13 +0900

    committing current work

---
+++ js/little.js
@@ -0,0 +1,80 @@
+// // image slide
+const siteList = [
+  "images/site-1.jpg",
+  "images/site-2.jpg",
+  "images/site-3.jpg",
+  "images/site-4-1.jpg",
+  "images/site-5-1.jpg",
+  "images/site-6-1.jpg",
+  "images/site-7-1.jpg",
+];
+
+let currentIndex = 0;
+
+function imgSlide() {
+  document.querySelector(".site-intro img").src = siteList[currentIndex];
+  currentIndex = (currentIndex + 1) % siteList.length;
+}
+
+setInterval(imgSlide, 1500);
+
+// clock
+const clockNumber = document.querySelector(".clock-number");
+
+function createElements(container, count, elementType) {
+  const elements = [];
+  for (let i = 1; i <= count; i++) {
+    elements.push(
+      `<span style="--index:${i}"><${elementType}>${i}</${elementType}></span>`
+    );
+  }
+  container.insertAdjacentHTML("afterbegin", elements.join(""));
+  return elements;
+}
+
+const numberElement = createElements(clockNumber, 12, "p");
+
+function getCurrentTime() {
+  const date = new Date();
+  const currentHour = date.getHours();
+  const currentMinute = date.getMinutes();
+  const currentSecond = date.getSeconds();
+  // console.log(currentHour);
+  // console.log(currentMinute);
+  // console.log(currentSecond);
+
+  const handHour = document.querySelector(".clock-hand .hour span");
+  const handMinute = document.querySelector(".clock-hand .minute span");
+  const handSecond = document.querySelector(".clock-hand .second span");
+
+  handHour.style.transform = `rotate(${
+    currentHour * 30 + currentMinute / 2
+  }deg)`;
+  handMinute.style.transform = `rotate(${currentMinute * 6}deg)`;
+  handSecond.style.transform = `rotate(${currentSecond * 6}deg)`;
+}
+
+setInterval(getCurrentTime, 1000);
+
+document.addEventListener("mousemove", (event) => {
+  const pointLight = document.querySelector(".point-light");
+  const x = event.pageX;
+  const y = event.pageY;
+  const pointLightRect = pointLight.getBoundingClientRect();
+  const rectWidth = pointLightRect.width;
+  const rectHeight = pointLightRect.height;
+
+  pointLight.style.left = x - rectWidth / 2 + "px";
+  pointLight.style.top = y - rectHeight / 2 + "px";
+  console.log("x", x);
+  console.log(rectWidth);
+});
+console.log("크기", window.innerWidth);
+
+function highlightPointLight(isHovered) {
+  const clockInner = document.querySelector(".clock-inner");
+  const pointLight = document.querySelector(".point-light");
+  clockInner.style.overflow = "hidden";
+  pointLight.style.background = isHovered ? "#fff" : "none";
+  pointLight.style.boxShadow = isHovered ? "0 0 1rem #fff" : "none";
+}
